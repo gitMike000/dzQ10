7b521357cffdfdb3ed8159a8ac57d3a3a0706194
gitMike000 <76992900+gitMike000@users.noreply.github.com>
2022-08-28 20:36:08 +0300

    Add files via upload

---
+++ boxer.h
@@ -0,0 +1,35 @@
+#ifndef BOXER_H
+#define BOXER_H
+
+#include <QFile>
+//#include <QDate>
+#include <QObject>
+
+    class BoxeR : public QObject
+{
+    Q_OBJECT
+public:
+    explicit BoxeR();
+    virtual ~BoxeR();
+    Q_INVOKABLE
+        void load();
+    Q_INVOKABLE
+        void nextRecord();
+    Q_INVOKABLE
+        void saveRecord(QString, QString, QString);
+    Q_INVOKABLE
+        int getNTasks();
+
+private:
+    QFile *file;
+    int ntasks = 0;
+
+signals:
+    void end(bool succed);
+    //void loadTask(QString task, QDate endTask, char progress);
+    void loadTask(QString task, QString endTask, QString progress);
+
+private slots:
+};
+
+#endif // BOXER_H
